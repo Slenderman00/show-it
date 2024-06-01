b6dcf1fe45c4320e060525d98a3ee9445da80733
Joar Heimonen <contact@joar.me>
2024-06-01 19:05:18 +0200

    fix: changed font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,42 @@
-import './App.css'
-import Grid from './components/grid/cssGrid'
-import { Distribute25252525, Distribute5050, Distribute2575, Distribute255025, Distribute100 } from './components/grid/distribution/gridDistribution'
-import Logo from './components/logo/logo'
-import Infotext from './components/infotext/infotext'
-import Navbar from './components/navbar/navbar'
-import Home from './pages/home'
+import "./App.css";
+import Grid from "./components/grid/cssGrid";
+import {
+  Distribute25252525,
+  Distribute5050,
+  Distribute2575,
+  Distribute255025,
+  Distribute100,
+} from "./components/grid/distribution/gridDistribution";
+import Logo from "./components/logo/logo";
+import Infotext from "./components/infotext/infotext";
+import Navbar from "./components/navbar/navbar";
+import Home from "./pages/home";
 
-import { useState } from 'react'
-
+import { useState } from "react";
+import Page1 from "./pages/page1";
 
 function App() {
-  const [view, setView] = useState(<Home/>)
+  const [view, setView] = useState(<Home />);
 
   return (
     <>
       <Grid>
         <Distribute2575
-          one={<Logo/>}
+          one={<Logo />}
           two={
-          <Navbar 
-            viewCallback={setView}
-            content={[{title: "Home", view: <Home/>}]}
-          />
-        }
+            <Navbar
+              viewCallback={setView}
+              content={[
+                { title: "Home", view: <Home /> },
+                { title: "Page 1", view: <Page1/>}
+              ]}
+            />
+          }
         />
         {view}
       </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
